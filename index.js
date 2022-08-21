3c2e65a3363ce34dfeb936adfcec763fc6ab9a15
+ <+>
2022-08-21 22:54:17 +0300

    done and all working

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName='Scuber';
+
+//declaring using let
+let mostProfitableNeighborhood; 
+mostProfitableNeighborhood ="Chelsea";
+
+let companyCeo; 
+companyCeo = "Susan Smith";
